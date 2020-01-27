09951bacbe2bb475463a0755c6529308c67d8d01
Walter Scarborough <wscarborough@pivotal.io>
2020-01-27 12:03:13 -0500

    Initial Commit

---
+++ TexasJargonGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TexasJargonGenerator
+//
+//  Created by pivotal on 1/27/20.
+//  Copyright © 2020 pivotal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
