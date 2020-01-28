2147f76c210f1ae82f54557c0474c325ecec1075
Walter Scarborough <wscarborough@pivotal.io>
2020-01-28 14:19:02 -0500

    Add test case for bad data and alternate happy path for JargonRepository

--- frontend/ios/TexasJargonGeneratorTests/JargonRepositoryTest.swift
+++ frontend/ios/TexasJargonGeneratorTests/JargonRepositoryTest.swift
@@ -15,15 +15,60 @@ class JargonRepositoryTest: XCTestCase {
 
         let jargonRepository = JargonRepository()
 
-        let expectedJargon = Jargon(
-            phrase: "dang"
-        )
-
         let subscriber = jargonRepository.fetchJargon(url: URL(string: "http://localhost:8080/jargon")!)
 
         subscriber.sink(
-            receiveCompletion: { error in
-                XCTFail("error is: \(error)")
+            receiveCompletion: { result in
+                switch result {
+                case .finished: do {
+                    break
+                }
+                case .failure: do {
+                    XCTFail()
+                }
+                }
+            }, receiveValue: { actualJargon in
+                let expectedJargon = Jargon(
+                    phrase: "dang"
+                )
+
+                XCTAssertEqual(actualJargon, expectedJargon)
+                expectationFinished.fulfill()
+            }
+        ).store(in: &subscriberSet!)
+
+        wait(for: [expectationFinished], timeout: 3)
+    }
+
+    func testFetchJargonReturnsOtherExpectedJargon() {
+        let expectationFinished = expectation(description: "finished")
+
+        let expectedJargon = Jargon(phrase: "gosh darn")
+
+        let someURL = URL(string: "http://localhost:8080/someNonexistentEndpoint")!
+        FakeURLProtocol.testURLs = [someURL: try! JSONEncoder().encode(expectedJargon)]
+        FakeURLProtocol.response = HTTPURLResponse(
+            url: someURL,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        let urlSession = FakeURLProtocol.createURLSession()
+
+        let jargonRepository = JargonRepository(urlSession: urlSession)
+
+        let subscriber = jargonRepository.fetchJargon(url: someURL)
+
+        subscriber.sink(
+            receiveCompletion: { result in
+                switch result {
+                case .finished: do {
+                    break
+                }
+                case .failure: do {
+                    XCTFail()
+                }
+                }
             }, receiveValue: { actualJargon in
                 XCTAssertEqual(actualJargon, expectedJargon)
                 expectationFinished.fulfill()
@@ -65,6 +110,42 @@ class JargonRepositoryTest: XCTestCase {
                 }
             }, receiveValue: { _ in
                 XCTFail("This test should have failed for a bad HTTP status code")
+            }
+        ).store(in: &subscriberSet!)
+
+        wait(for: [expectationFinished], timeout: 3)
+    }
+
+    func testFetchJargonFailsForBadData() {
+        let expectationFinished = expectation(description: "finished")
+
+        let someURL = URL(string: "http://localhost:8080/someNonexistentEndpoint")!
+        FakeURLProtocol.testURLs = [someURL: "yolo".data(using: .unicode)!]
+        FakeURLProtocol.response = HTTPURLResponse(
+            url: someURL,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        let urlSession = FakeURLProtocol.createURLSession()
+
+        let jargonRepository = JargonRepository(urlSession: urlSession)
+
+        let subscriber = jargonRepository.fetchJargon(url: someURL)
+
+        subscriber.sink(
+            receiveCompletion: { result in
+                switch result {
+                case let .failure(error): do {
+                    XCTAssertNotNil(error)
+                    expectationFinished.fulfill()
+                }
+                default: do {
+                    XCTFail("should fail for specific error")
+                }
+                }
+            }, receiveValue: { _ in
+                XCTFail()
             }
         ).store(in: &subscriberSet!)
 
