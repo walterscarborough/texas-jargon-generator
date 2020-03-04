483e4885ac0856a781c26e19a7cfcebcdf46980e
Walter Scarborough <wscarborough@pivotal.io>
2020-03-04 13:25:22 -0500

    Small android cleanup

--- frontends/android/app/src/main/cpp/native-lib.cpp
+++ frontends/android/app/src/main/cpp/native-lib.cpp
@@ -3,10 +3,10 @@
 
 #include "../../../../../../common/random-jargon-generator/src/random_jargon_generator_library.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_texasjargongenerator_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
+extern "C" JNIEXPORT jstring JNICALL Java_com_example_texasjargongenerator_MainActivity_stringFromJNI(
+    JNIEnv *env,
+    jobject /* this */
+) {
     std::string phrase = generate_phrase();
     return env->NewStringUTF(phrase.c_str());
 }
--- frontends/android/app/src/main/java/com/example/texasjargongenerator/MainActivity.kt
+++ frontends/android/app/src/main/java/com/example/texasjargongenerator/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
-            System.loadLibrary("native-lib")
         }
     }
 }
